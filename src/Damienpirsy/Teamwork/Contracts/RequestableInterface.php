6abcce6a693d9f8a8f3ce5dd330a9b8f5427f154
Matteo Vignoli <damien.pirsy@gmail.com>
2016-05-17 13:32:24 +0200

    first commit

--- src/Damienpirsy/Teamwork/Contracts/RequestableInterface.php
+++
@@ -1,6 +0,0 @@
-<?php  
-namespace Damienpirsy\Teamwork\Contracts; 
-
-interface RequestableInterface {
-
-}
--- src/Damienpirsy/Teamwork/Exceptions/ClassNotCreatedException.php
+++
@@ -1,4 +0,0 @@
-<?php  
-namespace Damienpirsy\Teamwork\Exceptions; 
-
-class ClassNotCreatedException extends \Exception {}
